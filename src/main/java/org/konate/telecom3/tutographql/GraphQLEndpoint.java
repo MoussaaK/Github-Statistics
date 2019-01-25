f1968458d3ab6a321a1a3d8f967cc776b43b4d2b
MoussaaK <moussakonate27@gmail.com>
2019-01-25 01:16:30 +0100

    GraphQL endpoint

---
+++ src/main/java/org/konate/telecom3/tutographql/GraphQLEndpoint.java
@@ -0,0 +1,28 @@
+package org.konate.telecom3.tutographql;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.SimpleGraphQLServlet;
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public GraphQLEndpoint() {
+        super(buildSchema());
+    }
+
+    private static GraphQLSchema buildSchema() {
+        LinkRepository linkRepository = new LinkRepository();
+        return SchemaParser.newParser()
+                .file("schema.graphqls")
+                .resolvers(new Query(linkRepository))
+                .build()
+                .makeExecutableSchema();
+    }
+}
+
