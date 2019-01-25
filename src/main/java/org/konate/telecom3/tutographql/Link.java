744b27b94c7074bf75bcdc9a6b86e9abfde30019
MoussaaK <moussakonate27@gmail.com>
2019-01-25 01:16:12 +0100

    Added Classes link to type in graphql schema

---
+++ src/main/java/org/konate/telecom3/tutographql/Link.java
@@ -0,0 +1,21 @@
+package org.konate.telecom3.tutographql;
+
+public class Link {
+    
+    private final String url;
+    private final String description;
+
+    public Link(String url, String description) {
+        this.url = url;
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
+
