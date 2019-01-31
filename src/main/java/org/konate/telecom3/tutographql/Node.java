0c24b1d96dabfe3cab9d0c4bc0ee65a549ed405f
Mousse <moussakonate27@gmail.com>
2019-01-31 22:33:16 +0100

    Added Node

---
+++ src/main/java/org/konate/telecom3/tutographql/Node.java
@@ -0,0 +1,24 @@
+package org.konate.telecom3.tutographql;
+
+public class Node {
+	private String name;
+	private String description;
+	public Node() {
+		super();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+}
