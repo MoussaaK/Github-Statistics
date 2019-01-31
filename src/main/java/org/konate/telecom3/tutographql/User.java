5e2650461dcdc74254ca06da57cbf39a37561622
Mousse <moussakonate27@gmail.com>
2019-01-31 22:34:09 +0100

    Added User

---
+++ src/main/java/org/konate/telecom3/tutographql/User.java
@@ -0,0 +1,32 @@
+package org.konate.telecom3.tutographql;
+
+public class User {
+	private String name;
+    private Repository repositories;
+    
+	public User() {
+		super();
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public Repository getRepositories() {
+		return repositories;
+	}
+
+	public void setRepositories(Repository repositories) {
+		this.repositories = repositories;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", repositories=" + repositories + "]";
+	}
+
+}
