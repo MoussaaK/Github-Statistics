44eab6c263f4e7dffecdcb18469f64e5a0351cf7
Mousse <moussakonate27@gmail.com>
2019-01-31 22:33:25 +0100

    Added Nodes

---
+++ src/main/java/org/konate/telecom3/tutographql/Nodes.java
@@ -0,0 +1,20 @@
+package org.konate.telecom3.tutographql;
+
+import java.util.List;
+
+public class Nodes {
+	private List<Node> nodes;
+
+	public Nodes() {
+		super();
+	}
+
+	public List<Node> getNodes() {
+		return nodes;
+	}
+
+	public void setNodes(List<Node> nodes) {
+		this.nodes = nodes;
+	}
+	
+}
