ca68029122db1faea389b63bfb447d482c7f1706
Mousse <moussakonate27@gmail.com>
2019-01-31 22:33:59 +0100

    added Repository

---
+++ src/main/java/org/konate/telecom3/tutographql/Repository.java
@@ -0,0 +1,18 @@
+package org.konate.telecom3.tutographql;
+
+public class Repository {
+	private Nodes node;
+
+	public Repository() {
+		super();
+	}
+
+	public Nodes getNode() {
+		return node;
+	}
+
+	public void setNode(Nodes node) {
+		this.node = node;
+	}
+	
+}
