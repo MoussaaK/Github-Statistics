4986bad24806e6891c7f37ef9e85cf65d30ba6e6
Aboubakar C. Sokoba <Aboubakar C. Sokoba@LAPTOP-F49HE6FJ>
2019-02-14 02:48:42 +0100

    Added comments Resolved the non-existing description field errors

--- src/main/java/org/konate/telecom3/tutographql/util/LinkRepository.java
+++ src/main/java/org/konate/telecom3/tutographql/util/LinkRepository.java
@@ -4,34 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.konate.telecom3.tutographql.model.Link;
 
+ //This is where the responses from the requests are going to be stored
 public class LinkRepository {
-    
-    private final List<Link> links;
 
-    public LinkRepository() {
-        links = new ArrayList<>();
-    }
+	private final List<Link> links;
 
-    public List<Link> getAllLinks() {
-    	
-    	JSONObject responseFromGithub = new JSONObject(Utility.getData().toString());
+	public LinkRepository() {
+		links = new ArrayList<>();
+	}
+
+	
+	//Processing the data retrieved from the JSON
+	//Then saving it
+	public List<Link> getAllLinks() {
+
+		JSONObject responseFromGithub = new JSONObject(Utility.getData().toString());
 		JSONArray jsonArray = responseFromGithub.getJSONObject("data")
-										  .getJSONObject("user")
-										  .getJSONObject("repositories")
-										  .getJSONArray("nodes");
-		
+				.getJSONObject("user")
+				.getJSONObject("repositories")
+				.getJSONArray("nodes");
+
 		for (int i = 0; i < jsonArray.length(); i++) {
-			saveLink(new Link(jsonArray.getJSONObject(i).getString("url"),
-							   jsonArray.getJSONObject(i).getString("description")));
+			try {
+				saveLink(new Link(jsonArray.getJSONObject(i).getString("url"),
+						jsonArray.getJSONObject(i).getString("description")));
+			}
+			catch(JSONException e){
+				e.printStackTrace();
+			}
 		}
-    	
-        return links;
-    }
-    
-    public void saveLink(Link link) {
-        links.add(link);
-    }
+
+		return links;
+	}
+
+	public void saveLink(Link link) {
+		links.add(link);
+	}
 }
