d05d519aafe9eacf3a569605b1aa42d39f55504a
Mousse <moussakonate27@gmail.com>
2019-02-15 03:03:37 +0100

    Uncommented fields

--- src/main/java/org/konate/telecom3/tutographql/model/User.java
+++ src/main/java/org/konate/telecom3/tutographql/model/User.java
@@ -1,41 +1,39 @@
 package org.konate.telecom3.tutographql.model;
 
 public class User {
-//	private String name;
-//    private Repository repositories;
-//    
+	private String name;
+	private Repository repositories;
+	private String login;
+
 	public User() {
 		super();
 	}
-//
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	
-//	public String getName() {
-//		return name;
-//	}
-//
-//	public Repository getRepositories() {
-//		return repositories;
-//	}
-//
-//	public void setRepositories(Repository repositories) {
-//		this.repositories = repositories;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "User [name=" + name + ", repositories=" + repositories + "]";
-//	}
 
+	public User(String login) {
+		this.login = login;
+	}
 	
-	private String login;
- 
-    public User(String login) {
-        this.login = login;
-    }
-	
+	public User(String login, String name) {
+		this.login = login;
+		this.name = name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Repository getRepositories() {
+		return repositories;
+	}
+
+	public void setRepositories(Repository repositories) {
+		this.repositories = repositories;
+	}
+
 	public String getLogin() {
 		return login;
 	}
@@ -43,6 +41,8 @@ public class User {
 	public void setLogin(String login) {
 		this.login = login;
 	}
-	
-	
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", repositories=" + repositories + "]";
+	}
 }
