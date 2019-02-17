e6cabda1a7e2c74493dab40f1a48cec62644dcee
Mousse <moussakonate27@gmail.com>
2019-02-17 23:33:03 +0100

    Added new method to fetch more Users

--- src/main/java/org/konate/telecom3/tutographql/util/Utility.java
+++ src/main/java/org/konate/telecom3/tutographql/util/Utility.java
@@ -7,6 +7,8 @@ import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,27 +17,26 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.konate.telecom3.tutographql.model.User;
 
 public class Utility {
-	
-	private static CloseableHttpClient client = null;
-	private static CloseableHttpResponse response = null;
-	
+
 	//Token read from a file for security constraints
 	private static String token = getToken("./token.txt").toString();
-	private static JSONObject GraphQLQuery = null; 
 
 	//Using http and the github api to fetch data using 
 	//Graphql langage. Saving it in a JSON object 
 	public static StringBuilder getQueryResponse(String queryString) {
-		client = HttpClients.createDefault();
+		CloseableHttpClient client = HttpClients.createDefault();
+		CloseableHttpResponse response = null;
+
 		HttpPost httpPost = new HttpPost("https://api.github.com/graphql");
 		httpPost.addHeader("Authorization", token);
 		httpPost.addHeader("Accept", "application/json");
-		
-		GraphQLQuery = new JSONObject();
-		
+
+		JSONObject GraphQLQuery = new JSONObject();
 		GraphQLQuery.put("query", queryString);
 
 		StringEntity entity = null;
@@ -46,6 +47,7 @@ public class Utility {
 			e.printStackTrace();
 		}
 		httpPost.setEntity(entity);
+
 		try {
 			response = client.execute(httpPost);
 		} catch (IOException e1) {
@@ -55,17 +57,23 @@ public class Utility {
 		try {
 			BufferedReader reader= new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 			String line = null;
-			
+
 			while((line = reader.readLine()) != null){
 				builder.append(line);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
+		// Ensure proper release of system resources
+		try {
+			response.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return builder;
-    }
-	
+	}
+
 	public static String getToken(String fileName) {
 		Path path = Paths.get(fileName);
 		try (Stream<String> lines = Files.lines(path)) {
@@ -74,5 +82,53 @@ public class Utility {
 			System.out.println(e.getMessage());
 		}
 		return null;
-	} 
+	}
+
+	public static List<User> getMoreUsers(JSONObject firstResonseFromGithub) {
+
+		boolean hasNextPage = firstResonseFromGithub.getJSONObject("data")
+				.getJSONObject("search")
+				.getJSONObject("pageInfo")
+				.getBoolean("hasNextPage");
+		String endCursor = firstResonseFromGithub.getJSONObject("data")
+				.getJSONObject("search")
+				.getJSONObject("pageInfo")
+				.getString("endCursor");
+
+		JSONArray array = null;
+		List<JSONArray> jsonArrays = new ArrayList<>();
+		List<User> moreUsers = new ArrayList<>();
+		String queryString;
+		
+		while (hasNextPage) {
+			queryString = "{ search(query: \"type:user\", first: 100, after:\"" + endCursor + "\", type: USER) { userCount pageInfo { endCursor hasNextPage } edges { node { ... on User { login name } } } } }";
+			JSONObject newResponseFromGithub = new JSONObject(Utility.getQueryResponse(queryString).toString());
+
+			hasNextPage = newResponseFromGithub.getJSONObject("data")
+					.getJSONObject("search")
+					.getJSONObject("pageInfo")
+					.getBoolean("hasNextPage");
+
+			endCursor = newResponseFromGithub.getJSONObject("data")
+					.getJSONObject("search")
+					.getJSONObject("pageInfo")
+					.getString("endCursor");
+
+			array = newResponseFromGithub.getJSONObject("data")
+					.getJSONObject("search")
+					.getJSONArray("edges");
+			//System.out.println("array -> " + array);
+			jsonArrays.add(array);
+		}
+		//System.out.println("jsonArrays ->" + jsonArrays);
+		
+		for (JSONArray jsonArray : jsonArrays) {
+			for (int i = 0; i < jsonArray.length(); i++) {
+				moreUsers.add(new User(jsonArray.getJSONObject(i).getJSONObject("node").getString("login"),
+						jsonArray.getJSONObject(i).getJSONObject("node").getString("name")));
+			}
+		}
+
+		return moreUsers;
+	}
 }
