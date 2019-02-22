9bdd108612b4a502ae2a87976baad82278899de2
Mousse <moussakonate27@gmail.com>
2019-02-22 03:15:18 +0100

    Added fields totalRepositoriesDiskUsage and avarageCommitCount to Company Class

--- src/main/java/org/konate/telecom3/tutographql/model/Company.java
+++ src/main/java/org/konate/telecom3/tutographql/model/Company.java
@@ -5,15 +5,23 @@ public class Company {
 	private String companyName;
 	private int numberOfMembers;
 	private int numberOfRepositories;
+	private int totalRepositoriesDiskUsage;
+	private int avarageCommitCount;
 
 	public Company() {
 		super();
 	}
 
-	public Company(String companyName, int numberOfMembers, int numberOfRepositories) {
+	public Company(String companyName,
+				   int numberOfMembers, 
+				   int numberOfRepositories,
+				   int totalRepositoriesDiskUsage,
+				   int avarageCommitCount) {
 		this.setCompanyName(companyName);
 		this.numberOfMembers = numberOfMembers;
 		this.numberOfRepositories = numberOfRepositories;
+		this.totalRepositoriesDiskUsage = totalRepositoriesDiskUsage;
+		this.avarageCommitCount = avarageCommitCount;
 	}
 
 	public int getNumberOfMembers() {
@@ -38,5 +46,21 @@ public class Company {
 
 	public void setCompanyName(String companyName) {
 		this.companyName = companyName;
+	}
+
+	public int getTotalRepositoriesDiskUsage() {
+		return totalRepositoriesDiskUsage;
+	}
+
+	public void setTotalRepositoriesDiskUsage(int totalRepositoriesDiskUsage) {
+		this.totalRepositoriesDiskUsage = totalRepositoriesDiskUsage;
+	}
+
+	public int getAvarageCommitCount() {
+		return avarageCommitCount;
+	}
+
+	public void setAvarageCommitCount(int avarageCommitCount) {
+		this.avarageCommitCount = avarageCommitCount;
 	}
 }
