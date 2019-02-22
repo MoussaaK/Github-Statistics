970cef611fffa16b8076639badc5f895f951f65b
Mousse <moussakonate27@gmail.com>
2019-02-22 03:18:17 +0100

    Updated nodes

--- src/main/java/org/konate/telecom3/tutographql/model/Node.java
+++ src/main/java/org/konate/telecom3/tutographql/model/Node.java
@@ -3,8 +3,15 @@ package org.konate.telecom3.tutographql.model;
 public class Node {
 	private String name;
 	private String description;
+	private String primaryLanguage;
+	
 	public Node() {
 		super();
+	}
+	public Node(String name, String primaryLanguage) {
+		super();
+		this.name = name;
+		this.primaryLanguage = primaryLanguage;
 	}
 	public String getName() {
 		return name;
@@ -17,5 +24,11 @@ public class Node {
 	}
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	public String getPrimaryLanguage() {
+		return primaryLanguage;
+	}
+	public void setPrimaryLanguage(String primaryLanguage) {
+		this.primaryLanguage = primaryLanguage;
 	}
 }
