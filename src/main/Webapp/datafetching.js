0b4fb4999fb1742f46a794288b31d0319dd3f8fa
Aboubakar C. Sokoba <Aboubakar C. Sokoba@LAPTOP-F49HE6FJ>
2019-02-22 19:10:38 +0100

    Deleting unused pages
    succeeded with first AJAX request ( a bit slow though because of
    window.onload)

--- src/main/Webapp/datafetching.js
+++ src/main/Webapp/datafetching.js
@@ -1,17 +1,20 @@
+window.onload = function () {
+	var resource_url = "http://localhost:8080/graphql?query={allUsersCount}";
+	$("document").ready(function(){
+		$.ajax({
+			contentType: "",
+			async: true,
+			method: "GET",
+			url: resource_url,
+			dataType: "json",
+			success: function(response) {
+				$("#numberOfUsers").text(response.data.allUsersCount.toString());
+			}
+		})
+	})
+}
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script>
-var resource_url = "http://localhost:8080/graphql?query={allUsersCount}";
-$.ajax({
-	contentType: "",
-	async: false,
-	method: "GET",
-	url: resource_url,
-	dataType: "json",
-	success: function(data) {
-		console.log(data);
-		$("numberOfUsers").
 
-</script>
+
 
 
