6a22d047aff49f4c4f64269ca5e7b1344e42217f
Aboubakar C. Sokoba <Aboubakar C. Sokoba@LAPTOP-F49HE6FJ>
2019-02-25 00:25:20 +0100

    Adapting the front-end to our needs

--- src/main/Webapp/datafetching.js
+++ src/main/Webapp/datafetching.js
@@ -24,6 +24,7 @@ document.onreadystatechange = function(e)
 				$.getJSON("http://localhost:8080/graphql?query={allClosedIssueCount}", {
 					format: "json"
 				})).then(function (res1, res2, res3, res4) {
+
 					$("#numberOfUsers").text(res1[0].data.allUsersCount.toLocaleString('en', {useGrouping:true}));
 					$("#numberOfRepositories").text(res2[0].data.allRepositoryCount.toLocaleString('en', {useGrouping:true}));
 					$("#numberOfOpenedIssues").text(res3[0].data.allOpenIssueCount.toLocaleString('en', {useGrouping:true}));
@@ -32,25 +33,33 @@ document.onreadystatechange = function(e)
 				});
 		
 		//For companies data
-//		$.when(
-//				$.getJSON("http://localhost:8080/graphql?query={allUsersCount}", { 
-//					format: "json"
-//				}),
-//				$.getJSON("http://localhost:8080/graphql?query={allRepositoryCount}", {
-//					format: "json"
-//				}),
-//				$.getJSON("http://localhost:8080/graphql?query={allOpenIssueCount}", {
-//					format: "json"
-//				}),
-//				$.getJSON("http://localhost:8080/graphql?query={allClosedIssueCount}", {
-//					format: "json"
-//				})).then(function (res1, res2, res3, res4) {
-//					$("#numberOfUsers").text(res1[0].data.allUsersCount.toLocaleString('en', {useGrouping:true}));
-//					$("#numberOfRepositories").text(res2[0].data.allRepositoryCount.toLocaleString('en', {useGrouping:true}));
-//					$("#numberOfOpenedIssues").text(res3[0].data.allOpenIssueCount.toLocaleString('en', {useGrouping:true}));
-//					$("#numberOfClosedIssues").text(res4[0].data.allClosedIssueCount.toLocaleString('en', {useGrouping:true}));
-//
-//				});
+		//Populating a table
+		
+		$.ajax({
+			contentType: "",
+			async: true,
+			method: "GET",
+			url: "http://localhost:8080/graphql?query={someCompaniesData{companyName,numberOfMembers,numberOfRepositories,avarageCommitCount,totalRepositoriesDiskUsage}}",
+			dataType: "json",
+			success: function(response) {
+				
+				var table = document.getElementById('companiesTable');
+				response.data.someCompaniesData.forEach(function(object) {
+	                var tr = document.createElement('tr');
+	                tr.className ="row100 head";
+	                tr.innerHTML = '<td>' + object.companyName + '</td>' 
+	                +
+	                '<td>' + object.numberOfMembers + '</td>' +
+	                '<td>' + object.numberOfRepositories + '</td>' +
+	                '<td>' + object.avarageCommitCount + '</td>' 
+	                +'<td>' + object.totalRepositoriesDiskUsage.toLocaleString('en', {useGrouping:true}) + ' bytes'+'</td>'
+	                ;
+	                table.appendChild(tr);
+	            });
+			}
+		})
+		
+
 	}
 };
 
