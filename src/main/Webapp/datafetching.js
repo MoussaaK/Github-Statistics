94aa8a78f7b255090ff9b656b83b1bcfdb7557a5
Aboubakar C. Sokoba <Aboubakar C. Sokoba@LAPTOP-F49HE6FJ>
2019-02-26 10:03:35 +0100

    Added Pie chart for languages. Deleted unused html page

--- src/main/Webapp/datafetching.js
+++ src/main/Webapp/datafetching.js
@@ -1,4 +1,30 @@
+function convertJsonIntoArray(jsonArray){
+	var resultArray = [];
+	var arrayHeader = ['Languages','Number of Repositories'];
+	resultArray.push(arrayHeader);
+	for(var obj in jsonArray){
+		var fieldArray = [];
+		fieldArray.push(jsonArray[obj].name);
+		fieldArray.push(jsonArray[obj].languageFrequency);
+		resultArray.push(fieldArray);
+	}
+	return resultArray;
+}
 
+function drawLanguagesPieChart(array){
+	google.charts.load("current", {packages:["corechart"]});
+	google.charts.setOnLoadCallback(drawChart);
+	function drawChart() {
+		var data = google.visualization.arrayToDataTable(array);
+
+        var options = {
+                title: 'Languages on Github'
+              };
+
+		var chart = new google.visualization.PieChart(document.getElementById('langagesPieChart'));
+		chart.draw(data, options);
+	}
+}
 
 
 
@@ -8,6 +34,20 @@ document.onreadystatechange = function(e)
 {
 	if (document.readyState === 'complete')
 	{
+		$.ajax({
+			contentType: "",
+			async: true,
+			method: "GET",
+			url: "http://localhost:8080/graphql?query={someLanguages{name,languageFrequency}}",
+			dataType: "json",
+			success: function(response) {
+				var usefulResponse = response.data.someLanguages;
+				drawLanguagesPieChart(convertJsonIntoArray(usefulResponse));		
+
+			}
+		})
+
+
 		//dom is ready, window.onload fires later
 		//Doing all Ajax simultaneously
 		//For the top page data
@@ -23,18 +63,21 @@ document.onreadystatechange = function(e)
 				}),
 				$.getJSON("http://localhost:8080/graphql?query={allClosedIssueCount}", {
 					format: "json"
-				})).then(function (res1, res2, res3, res4) {
+				}),
+				$.getJSON("http://localhost:8080/graphql?query={someLanguages{name,languageFrequency}}", {
+					format: "json"
+				})).then(function (res1, res2, res3, res4,res5 ) {
 
 					$("#numberOfUsers").text(res1[0].data.allUsersCount.toLocaleString('en', {useGrouping:true}));
 					$("#numberOfRepositories").text(res2[0].data.allRepositoryCount.toLocaleString('en', {useGrouping:true}));
 					$("#numberOfOpenedIssues").text(res3[0].data.allOpenIssueCount.toLocaleString('en', {useGrouping:true}));
 					$("#numberOfClosedIssues").text(res4[0].data.allClosedIssueCount.toLocaleString('en', {useGrouping:true}));
-
+					drawLanguagesPieChart(convertJsonIntoArray((res5[0]).data.someLanguages));
 				});
-		
+
 		//For companies data
 		//Populating a table
-		
+
 		$.ajax({
 			contentType: "",
 			async: true,
@@ -42,23 +85,23 @@ document.onreadystatechange = function(e)
 			url: "http://localhost:8080/graphql?query={someCompaniesData{companyName,numberOfMembers,numberOfRepositories,avarageCommitCount,totalRepositoriesDiskUsage}}",
 			dataType: "json",
 			success: function(response) {
-				
+
 				var table = document.getElementById('companiesTable');
 				response.data.someCompaniesData.forEach(function(object) {
-	                var tr = document.createElement('tr');
-	                tr.className ="row100 head";
-	                tr.innerHTML = '<td>' + object.companyName + '</td>' 
-	                +
-	                '<td>' + object.numberOfMembers + '</td>' +
-	                '<td>' + object.numberOfRepositories + '</td>' +
-	                '<td>' + object.avarageCommitCount + '</td>' 
-	                +'<td>' + object.totalRepositoriesDiskUsage.toLocaleString('en', {useGrouping:true}) + ' bytes'+'</td>'
-	                ;
-	                table.appendChild(tr);
-	            });
+					var tr = document.createElement('tr');
+					tr.className ="row100 head";
+					tr.innerHTML = '<td>' + ' ' + object.companyName + '</td>' 
+					+
+					'<td>' + object.numberOfMembers + '</td>' +
+					'<td>' + object.numberOfRepositories + '</td>' +
+					'<td>' + object.avarageCommitCount + '</td>' 
+					+'<td>' + object.totalRepositoriesDiskUsage.toLocaleString('en', {useGrouping:true}) + ' bytes'+'</td>'
+					;
+					table.appendChild(tr);
+				});
 			}
 		})
-		
+
 
 	}
 };
